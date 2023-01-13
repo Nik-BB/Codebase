b0dc7c4f1eab6ef703ea1d68d8a714fe7c8245f1
Nik <nik@niks-mbp.wkstn.qmul.ac.uk>
2023-01-13 13:02:24 +0000

    Add drug blind train test split

--- DRP_nb/splitting.py
+++ DRP_nb/splitting.py
@@ -6,47 +6,90 @@ do not overlap between the train test and val sets.
 
 import sklearn
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def split(seed, _all_cls, _all_drugs, all_targets, train_size=0.8):
-    '''Train test split for cancer blind testing with checks. (no val set)
+def split(seed, _all_cls, _all_drugs, all_targets, train_size=0.8, 
+          split_type='cblind'):
+    '''Train test split for cancer or drug blind testing (no val set)
     
     Cancer blind testing means cell lines do not overlap between
     the train test and val sets. 
+    Drug blind testing means drugs do not overlap between
+    the train test and val sets. 
     '''
-    train_cls, test_cls = train_test_split(_all_cls, train_size=train_size, 
+    #input cheks
+    if type(_all_drugs) == type(_all_cls) != pd.Index:
+        print('_all_drugs and _all_cls need to be PD indxes')
+
+    #cancer blind splitting
+    if split_type == 'cblind': 
+        train_cls, test_cls = train_test_split(_all_cls, train_size=train_size, 
+                                               random_state=seed)
+
+        assert len(set(train_cls).intersection(test_cls)) == 0
+
+        frac_train_cl = len(train_cls) / len(_all_cls)
+        frac_test_cl = len(test_cls) / len(_all_cls)
+
+        print('Fraction of cls in sets, relative to all cls'\
+              'before mising values are removed')          
+        print(f'train fraction {frac_train_cl}, test fraction {frac_test_cl}')
+        print('------')
+
+
+        #add in the drugs to each cell line. 
+        def create_cl_drug_pair(cells):
+            all_pairs = []
+            for drug in _all_drugs:
+                pairs = cells + '::' + drug
+                #only keep pair if there is a truth value for it
+                for pair in pairs:
+                    if pair in all_targets:
+                        all_pairs.append(pair)
+
+
+            return np.array(all_pairs)
+
+        train_pairs = create_cl_drug_pair(train_cls)
+        test_pairs = create_cl_drug_pair(test_cls)
+        
+    #drug blind splitting    
+    if split_type == 'dblind':
+        train_ds, test_ds = train_test_split(_all_drugs, train_size=train_size, 
                                            random_state=seed)
-          
-    assert len(set(train_cls).intersection(test_cls)) == 0
-    
-    frac_train_cl = len(train_cls) / len(_all_cls)
-    frac_test_cl = len(test_cls) / len(_all_cls)
 
-    print('Fraction of cls in sets, relative to all cls'\
-          'before mising values are removed')          
-    print(f'train fraction {frac_train_cl}, test fraction {frac_test_cl}')
-    print('------')
+        assert len(set(train_ds).intersection(test_ds)) == 0
 
-    
-    #add in the drugs to each cell line. 
-    def create_cl_drug_pair(cells):
-        all_pairs = []
-        for drug in _all_drugs:
-            pairs = cells + '::' + drug
-            #only keep pair if there is a truth value for it
-            for pair in pairs:
-                if pair in all_targets:
-                    all_pairs.append(pair)
+        frac_train_ds = len(train_ds) / len(_all_drugs)
+        frac_test_ds = len(test_ds) / len(_all_drugs)
+
+        print('Fraction of drugs in sets, relative to all drugs'\
+              'before mising values are removed')          
+        print(f'train fraction {frac_train_ds}, test fraction {frac_test_ds}')
+        print('------')
+
+        #add in the drugs to each cell line. 
+        def create_cl_drug_pair(drugs):
+            all_pairs = []
+            for cell in _all_cls:
+                pairs = cell + '::' + drugs
+                #only keep pair if there is a truth value for it
+                for pair in pairs:
+                    if pair in all_targets:
+                        all_pairs.append(pair)
 
 
-        return np.array(all_pairs)
+            return np.array(all_pairs)
 
-    train_pairs = create_cl_drug_pair(train_cls)
-    test_pairs = create_cl_drug_pair(test_cls)
+        train_pairs = create_cl_drug_pair(train_ds)
+        test_pairs = create_cl_drug_pair(test_ds)
+
 
     train_pairs = sklearn.utils.shuffle(train_pairs, random_state=seed)
     test_pairs = sklearn.utils.shuffle(test_pairs, random_state=seed)
+          
 
     assert len(set(train_pairs).intersection(test_pairs)) == 0
 
@@ -60,29 +103,51 @@ def split(seed, _all_cls, _all_drugs, all_targets, train_size=0.8):
           f'{frac_test_pairs}')
 
     #checking split works as expected.  
+ 
+    if split_type =='cblind':   
     #create mapping of cls to cl drug pairs for test train and val set. 
-    def create_cl_to_pair_dict(cells):
-        '''Maps a cell line to all cell line drug pairs with truth values
+        def create_cl_to_pair_dict(cells):
+            '''Maps a cell line to all cell line drug pairs with truth values
+
+            '''
+            dic = {}
+            for cell in cells:
+                dic[cell] = []
+                for drug in _all_drugs:
+                    pair = cell + '::' + drug
+                    #filters out pairs without truth values
+                    if pair in all_targets:
+                        dic[cell].append(pair)
+            return dic
+
+            train_cl_to_pair = create_cl_to_pair_dict(train_cls)
+            test_cl_to_pair = create_cl_to_pair_dict(test_cls)
+            #check right number of cls 
+            assert len(train_cl_to_pair) == len(train_cls)
+            assert len(test_cl_to_pair) == len(test_cls)
+
+    if split_type == 'dblind':
+        def create_cl_to_pair_dict(drugs):
+            '''Maps a drug to all cell line drug pairs with truth values
+
+            '''
+            dic = {}
+            for drug in drugs:
+                dic[drug] = []
+                for cell in _all_cls:
+                    pair = cell + '::' + drug
+                    #filters out pairs without truth values
+                    if pair in all_targets:
+                        dic[drug].append(pair)
+            return dic
         
-        '''
-        dic = {}
-        for cell in cells:
-            dic[cell] = []
-            for drug in _all_drugs:
-                pair = cell + '::' + drug
-                #filters out pairs without truth values
-                if pair in all_targets:
-                    dic[cell].append(pair)
-        return dic
-
-    train_cl_to_pair = create_cl_to_pair_dict(train_cls)
-    test_cl_to_pair = create_cl_to_pair_dict(test_cls)
-
-    #checks 
-    #check right number of cls 
-    assert len(train_cl_to_pair) == len(train_cls)
-    assert len(test_cl_to_pair) == len(test_cls)
-
+        train_cl_to_pair = create_cl_to_pair_dict(train_ds)
+        test_cl_to_pair = create_cl_to_pair_dict(test_ds)
+        #check right number of drugs 
+        assert len(train_cl_to_pair) == len(train_ds)
+        assert len(test_cl_to_pair) == len(test_ds)
+    
+    # more checks 
     #unpack dict check no overlap and correct number 
     def flatten(l):
         return [item for sublist in l for item in sublist]
