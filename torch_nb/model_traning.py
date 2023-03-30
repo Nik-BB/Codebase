60952bbbb562ab242d1ffa6bc831144638a578ba
Nik <nik@niks-mbp.wkstn.qmul.ac.uk>
2023-03-30 16:43:30 +0100

    Added train loop for arbitrary number of data loaders

--- torch_nb/model_traning.py
+++ torch_nb/model_traning.py
@@ -64,3 +64,70 @@ def train_loop(train_dl=None, val_dl=None, model=None, loss_fn=nn.MSELoss(),
                 print(f'Train loss: {ftl:>5f}')
                 
     return train_hist
+
+
+#train loop for mutiple dataloaders 
+def tl_multi_dls(train_dls=None, y_train=None, val_dls=None, y_val=None, model=None, loss_fn=nn.MSELoss(), 
+               optimiser=None, epochs=10, verb=1):
+    '''torch train loop for two data loaders
+    
+    ------inputs------
+    train_dls: iterable 
+    contains multiple inputs where each input is a data loader
+    
+    '''
+    train_hist = {'train_loss': [], 'val_loss': []}
+    assert type(train_dls) == list
+    
+    for e in range(epochs):
+        #train_dls_y = train_dls
+        #train_dls_y.append(y_train)
+        loss_train = 0.0
+        model.train()
+        for batch, (*x, y) in enumerate(zip(*train_dls, y_train)):
+            x = [inp.to(device) for inp in x]
+            y = y.to(device)
+
+            # Compute prediction error
+            pred = model(*x)
+            loss = loss_fn(pred, y)
+
+            # Backpropagation
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+            
+            loss_train += loss.item()
+            
+        train_hist['train_loss'].append(loss_train / len(train_dls[0]))
+        
+        if val_dls:
+            #val_dls_y = val_dls
+            #val_dls_y.append(y_val)
+            #find validaiton loss
+            val_loss = 0.0
+            r2 = 0.0
+            mse_val = 0.0 
+
+            model.eval()   
+            with torch.no_grad():
+                for *xv, yv in zip(*val_dls, y_val):
+                    xv = [inp.to(device) for inp in xv]
+                    yv = yv.to(device)
+                    val_pred = model(*xv)
+                    #val_pred = val_pred.reshape(len(val_pred))
+                    v_loss = loss_fn(val_pred, yv)
+                    val_loss += v_loss.item()
+                    yv = yv.cpu().detach().numpy()
+                    val_pred = val_pred.cpu().detach().numpy()
+            train_hist['val_loss'].append(val_loss / len(val_dls[0]))
+
+        if verb > 0:
+            print(f'Epoch {e + 1}\n-----------------')
+            ftl = loss_train / len(train_dls[0])
+            if val_dls:
+                fvl = val_loss / len(val_dls[0])
+                print(f'Train loss: {ftl:>5f}, Val loss: {fvl:>5f}')
+            else:
+                print(f'Train loss: {ftl:>5f}')
+    return train_hist
